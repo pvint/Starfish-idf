f2f2f6a2ba9ac26597e85ca2c5f143c1492eca53
Paul Vint <pjvint@gmail.com>
2019-04-04 20:37:32 -0400

    Added bluetooth SPP (not working)

--- main/starfish_main.cpp
+++ main/starfish_main.cpp
@@ -19,6 +19,14 @@
 #include "fauxmoESP.h"
 #include "driver/ledc.h"
 
+#include "esp_bt.h"
+#include "esp_bt_main.h"
+#include "esp_gap_bt_api.h"
+#include "esp_bt_device.h"
+#include "esp_spp_api.h"
+
+#include "cJSON.h"
+
 #include "lwip/err.h"
 #include "lwip/sys.h"
 
@@ -40,6 +48,13 @@ static EventGroupHandle_t s_wifi_event_group;
 const int WIFI_CONNECTED_BIT = BIT0;
 
 static const char *TAG = "starfish";
+static const char *SPP_TAG = "starfish-SPP";
+
+#define SPP_TAG "STARFISH_SPP"
+#define SPP_SERVER_NAME "SPP_SERVER"
+#define DEVICE_NAME "Starfish"
+static void parseJson(cJSON *root);
+
 
 static int s_retry_num = 0;
 
@@ -92,6 +107,156 @@ int ledValue[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 byte resolution = 12;
 byte ledDelay = 1;
 int pwmFadeTime = 2000;
+
+// Bluetooth
+static const esp_spp_mode_t esp_spp_mode = ESP_SPP_MODE_CB;
+
+static struct timeval time_new, time_old;
+static long data_num = 0;
+
+static const esp_spp_sec_t sec_mask = ESP_SPP_SEC_AUTHENTICATE;
+static const esp_spp_role_t role_slave = ESP_SPP_ROLE_SLAVE;
+
+static void esp_spp_cb(esp_spp_cb_event_t event, esp_spp_cb_param_t *param)
+{
+    switch (event) {
+    case ESP_SPP_INIT_EVT:
+	{
+        ESP_LOGI(SPP_TAG, "ESP_SPP_INIT_EVT");
+        // Get MAC
+        uint8_t mac[6];
+        esp_efuse_mac_get_default(mac);
+        char *d = (char*) malloc(strlen(DEVICE_NAME) + 4);
+        sprintf(d, "%s %02hhX", DEVICE_NAME, mac[5]);
+        esp_bt_dev_set_device_name(d);
+        free(d);
+
+        esp_bt_gap_set_scan_mode(ESP_BT_CONNECTABLE, ESP_BT_GENERAL_DISCOVERABLE);
+        esp_spp_start_srv(sec_mask,role_slave, 0, SPP_SERVER_NAME);
+        break;
+	}
+    case ESP_SPP_DISCOVERY_COMP_EVT:
+        {
+	ESP_LOGI(SPP_TAG, "ESP_SPP_DISCOVERY_COMP_EVT");
+        break;
+	}
+    case ESP_SPP_OPEN_EVT:
+        {
+	ESP_LOGI(SPP_TAG, "ESP_SPP_OPEN_EVT");
+        break;
+	}
+    case ESP_SPP_CLOSE_EVT:
+        {
+	ESP_LOGI(SPP_TAG, "ESP_SPP_CLOSE_EVT");
+        break;
+	}
+    case ESP_SPP_START_EVT:
+        {
+	ESP_LOGI(SPP_TAG, "ESP_SPP_START_EVT");
+        break;
+	}
+    case ESP_SPP_CL_INIT_EVT:
+        {
+	ESP_LOGI(SPP_TAG, "ESP_SPP_CL_INIT_EVT");
+        break;
+	}
+    case ESP_SPP_DATA_IND_EVT:
+        {
+	if (param->data_ind.len == 1 && *param->data_ind.data == '.')
+          return;
+        ESP_LOGI(SPP_TAG, "ESP_SPP_DATA_IND_EVT len=%d handle=%d",
+                 param->data_ind.len, param->data_ind.handle);
+        esp_log_buffer_hex("",param->data_ind.data,param->data_ind.len);
+
+        char *rcv = (char *)calloc(param->data_ind.len + 1, 1);
+        memcpy(rcv, param->data_ind.data, param->data_ind.len);
+        ESP_LOGI(SPP_TAG,"%s\n", rcv);
+
+        // Test to see if it is valid JSON
+        cJSON *json = cJSON_Parse(rcv);
+        // Causes crash if not valid JSON char *prt = cJSON_Print(json);
+        if ( json != NULL )
+          parseJson(json);
+        free(rcv);
+        break;
+	}
+    case ESP_SPP_CONG_EVT:
+        {
+	ESP_LOGI(SPP_TAG, "ESP_SPP_CONG_EVT");
+        break;
+	}
+    case ESP_SPP_WRITE_EVT:
+        {
+	ESP_LOGI(SPP_TAG, "ESP_SPP_WRITE_EVT");
+        break;
+	}
+
+    case ESP_SPP_SRV_OPEN_EVT:
+        {
+	ESP_LOGI(SPP_TAG, "ESP_SPP_SRV_OPEN_EVT");
+        gettimeofday(&time_old, NULL);
+        break;
+	}
+    default:
+    	{
+        break;
+	}
+    }
+}
+
+
+void esp_bt_gap_cb(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t *param)
+{
+    switch (event) {
+    case ESP_BT_GAP_AUTH_CMPL_EVT:{
+        if (param->auth_cmpl.stat == ESP_BT_STATUS_SUCCESS) {
+            ESP_LOGI(SPP_TAG, "authentication success: %s", param->auth_cmpl.device_name);
+            esp_log_buffer_hex(SPP_TAG, param->auth_cmpl.bda, ESP_BD_ADDR_LEN);
+        } else {
+            ESP_LOGE(SPP_TAG, "authentication failed, status:%d", param->auth_cmpl.stat);
+        }
+        break;
+    }
+    case ESP_BT_GAP_PIN_REQ_EVT:{
+        ESP_LOGI(SPP_TAG, "ESP_BT_GAP_PIN_REQ_EVT min_16_digit:%d", param->pin_req.min_16_digit);
+        if (param->pin_req.min_16_digit) {
+            ESP_LOGI(SPP_TAG, "Input pin code: 0000 0000 0000 0000");
+            esp_bt_pin_code_t pin_code = {0};
+            esp_bt_gap_pin_reply(param->pin_req.bda, true, 16, pin_code);
+        } else {
+            ESP_LOGI(SPP_TAG, "Input pin code: 1234");
+            esp_bt_pin_code_t pin_code;
+            pin_code[0] = '1';
+            pin_code[1] = '2';
+            pin_code[2] = '3';
+            pin_code[3] = '4';
+            esp_bt_gap_pin_reply(param->pin_req.bda, true, 4, pin_code);
+        }
+        break;
+    }
+#if (CONFIG_BT_SSP_ENABLED == true)
+    case ESP_BT_GAP_CFM_REQ_EVT:
+        ESP_LOGI(SPP_TAG, "ESP_BT_GAP_CFM_REQ_EVT Please compare the numeric value: %d", param->cfm_req.num_val);
+        esp_bt_gap_ssp_confirm_reply(param->cfm_req.bda, true);
+        break;
+    case ESP_BT_GAP_KEY_NOTIF_EVT:
+        ESP_LOGI(SPP_TAG, "ESP_BT_GAP_KEY_NOTIF_EVT passkey:%d", param->key_notif.passkey);
+        break;
+    case ESP_BT_GAP_KEY_REQ_EVT:
+        ESP_LOGI(SPP_TAG, "ESP_BT_GAP_KEY_REQ_EVT Please enter passkey!");
+        break;
+#endif
+
+    default: {
+        ESP_LOGI(SPP_TAG, "event: %d", event);
+        break;
+    }
+    }
+    return;
+}
+
+
+
 
 
 static esp_err_t event_handler(void *ctx, system_event_t *event)
@@ -150,6 +315,23 @@ void wifi_init_sta()
 	ESP_LOGI(TAG, "wifi_init_sta finished.");
 	ESP_LOGI(TAG, "connect to ap SSID:%s password:%s",
 			 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+}
+
+static void parseJson(cJSON *root)
+{
+	const cJSON *ch = NULL;
+	const cJSON *dc = NULL;
+
+	ch = cJSON_GetObjectItemCaseSensitive(root, "ch");
+	dc = cJSON_GetObjectItemCaseSensitive(root, "dc");
+
+	if (cJSON_IsNumber(ch) && cJSON_IsNumber(dc))
+	{
+		ESP_LOGI(TAG, "Got ch=%d & dc=%d\n", ch->valueint, dc->valueint);
+		//ledc_set_duty(LEDC_HS_MODE, ch->valueint, dc->valueint);
+		//ledc_update_duty(LEDC_HS_MODE,ch->valueint);
+		//setPWM(ch->valueint, dc->valueint, 0);
+	}
 }
 
 void connectWifi()
@@ -245,6 +427,8 @@ extern "C" void app_main()
 	});
 		
 
+	connectWifi();
+
 
 	//Initialize NVS
 	esp_err_t ret = nvs_flash_init();
@@ -256,7 +440,60 @@ extern "C" void app_main()
 	
 	//ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
 	//wifi_init_sta();
-	connectWifi();
+
+	// init Bluetooth
+
+    esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+
+    if ((ret = esp_bt_controller_init(&bt_cfg)) != ESP_OK) {
+        ESP_LOGE(SPP_TAG, "%s initialize controller failed: %s\n", __func__, esp_err_to_name(ret));
+        return;
+    }
+
+    if ((ret = esp_bt_controller_enable(ESP_BT_MODE_CLASSIC_BT)) != ESP_OK) {
+        ESP_LOGE(SPP_TAG, "%s enable controller failed: %s\n", __func__, esp_err_to_name(ret));
+        return;
+    }
+
+	ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_BLE));
+
+    if ((ret = esp_bluedroid_init()) != ESP_OK) {
+        ESP_LOGE(SPP_TAG, "%s initialize bluedroid failed: %s\n", __func__, esp_err_to_name(ret));
+        return;
+    }
+
+    if ((ret = esp_bluedroid_enable()) != ESP_OK) {
+        ESP_LOGE(SPP_TAG, "%s enable bluedroid failed: %s\n", __func__, esp_err_to_name(ret));
+        return;
+    }
+    if ((ret = esp_bt_gap_register_callback(esp_bt_gap_cb)) != ESP_OK) {
+        ESP_LOGE(SPP_TAG, "%s gap register failed: %s\n", __func__, esp_err_to_name(ret));
+        return;
+    }
+
+    if ((ret = esp_spp_register_callback(esp_spp_cb)) != ESP_OK) {
+        ESP_LOGE(SPP_TAG, "%s spp register failed: %s\n", __func__, esp_err_to_name(ret));
+        return;
+    }
+
+    if ((ret = esp_spp_init(esp_spp_mode)) != ESP_OK) {
+        ESP_LOGE(SPP_TAG, "%s spp init failed: %s\n", __func__, esp_err_to_name(ret));
+        return;
+    }
+#if (CONFIG_BT_SSP_ENABLED == true)
+    /* Set default parameters for Secure Simple Pairing */
+    esp_bt_sp_param_t param_type = ESP_BT_SP_IOCAP_MODE;
+    esp_bt_io_cap_t iocap = ESP_BT_IO_CAP_IO;
+    esp_bt_gap_set_security_param(param_type, &iocap, sizeof(uint8_t));
+#endif
+
+    /*
+     * Set default parameters for Legacy Pairing
+     * Use variable pin, input pin code when pairing
+     */
+    esp_bt_pin_type_t pin_type = ESP_BT_PIN_TYPE_VARIABLE;
+    esp_bt_pin_code_t pin_code;
+    esp_bt_gap_set_pin(pin_type, 0, pin_code);
 
 
 
